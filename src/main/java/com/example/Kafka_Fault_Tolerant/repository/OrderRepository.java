a923dfd269a12205272fe33e2cdf396678f49156
bharathdayal <bharathdayals@gmail.com>
2025-12-02 19:47:43 +0530

    Delete src directory

--- src/main/java/com/example/Kafka_Fault_Tolerant/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Kafka_Fault_Tolerant.repository;
-
-import com.example.Kafka_Fault_Tolerant.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Long> {
-}
--- src/main/java/com/example/Kafka_Fault_Tolerant/service/OrderServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.Kafka_Fault_Tolerant.service;
-
-import com.example.Kafka_Fault_Tolerant.dto.OrderEvent;
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-public interface OrderServiceImpl {
-    void CreateOrderAndPublish(OrderEvent event) throws JsonProcessingException;
-}
